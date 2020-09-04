a20d24c7e3313f15e6381cc6602ceb0aeac520a2
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-03 19:22:54 -0600

    pull friends information from server with token

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import Friends from "./components/Friends";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <h1>Hello world</h1>
         <Link to="/login">Login</Link>
+        <br />
         <Link to="/friends">Friends</Link>
       </div>
 
@@ -16,7 +18,7 @@ function App() {
         <Route path="/login">
           <Login />
         </Route>
-        <PrivateRoute path="/friends" component="Friends" />
+        <PrivateRoute path="/friends" component={Friends} />
       </Switch>
     </Router>
   );
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Friends = () => {
-  return <h1>Hello Friends</h1>;
+  const [friends, setFriends] = useState([]);
+
+  const getFriends = () => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/friends")
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  // Get all friends from the API /friends
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  // TODO Add for to create new friend
+  return (
+    <div>
+      <h1>Hello Friends</h1>
+      {friends.map((friend) => {
+        return (
+          <div key={friend.id}>
+            <h3>{friend.name}</h3>
+            <p>{friend.email}</p>
+            <p>{friend.age}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Friends;
